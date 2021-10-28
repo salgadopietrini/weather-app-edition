738996886b5abad48fd04d46bae9bf462faca535
ManuelSalgado <manuel.salgado@bootcamp-upgrade.com>
2021-10-28 14:41:37 +0100

    add existing key

--- src/helpers/fetchData.js
+++ src/helpers/fetchData.js
@@ -1,11 +1,13 @@
 const fetchData = async (location, days) => {
-    try {
-        const response = await fetch(`https://api.weatherapi.com/v1/forecast.json?key=${process.env.REACT_APP_API_KEY}&q=${location}&days=${days}&aqi=no&alerts=no`);
-        const data = await response.json();
-        return data;
-    } catch (e) {
-        console.error(e);
-    }
+  try {
+    const response = await fetch(
+      `https://api.weatherapi.com/v1/forecast.json?key=52371a4a6db84b44b26132325212810&q=${location}&days=${days}&aqi=no&alerts=no`
+    );
+    const data = await response.json();
+    return data;
+  } catch (e) {
+    console.error(e);
+  }
 };
 
 export default fetchData;
