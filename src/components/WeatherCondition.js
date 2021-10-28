e87a1cb3c13db064a889b188b3ebcbbe0abff88a
ManuelSalgado <manuel.salgado@bootcamp-upgrade.com>
2021-10-28 16:52:07 +0100

    add local time to weather description

--- src/components/WeatherCondition.js
+++ src/components/WeatherCondition.js
@@ -1,18 +1,21 @@
-import React from 'react';
-import '../styles/CurrentForecast.css';
-import {connect} from 'react-redux';
+import React from "react";
+import "../styles/CurrentForecast.css";
+import { connect } from "react-redux";
 
 const WeatherCondition = (props) => {
-    return (
-        <div className='condition'>it's {props.condition}</div>
-    )
-}
+  return (
+    <div className="condition">
+      it's {props.condition} and the local time is {props.localTime.slice(-5)}
+      {props.localTime.slice(-5, -3) < 12 ? "am" : "pm"}
+    </div>
+  );
+};
 
 const mapStateToProps = (state) => ({
-    location: state.weatherData.location,
-    responseLocation: state.weatherData.responseLocation,
-    condition: state.weatherData.condition,
-})
+  location: state.weatherData.location,
+  responseLocation: state.weatherData.responseLocation,
+  condition: state.weatherData.condition,
+  localTime: state.weatherData.localTime,
+});
 
 export default connect(mapStateToProps)(WeatherCondition);
-
